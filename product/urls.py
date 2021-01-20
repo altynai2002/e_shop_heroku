1454fdb4107a44958f9fbe4c32a276551a6e83ec
altynai2002 <altynay.bakytbekova.02@mail.ru>
2021-01-20 21:05:07 +0600

    first commit

---
+++ product/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import ProductViewSet, CategoriesList
+
+
+router = DefaultRouter()
+router.register('', ProductViewSet)
+
+# products = ProductViewSet.as_view({
+#     'get': 'list',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'post': 'create',
+#     'delete': 'destroy'
+# })
+
+urlpatterns = [
+    # path('', ProductsList.as_view()),
+    # path('detail/<str:pk>/', ProductDetail.as_view()),
+    path('categories/', CategoriesList.as_view()),
+
+    # path('', products),
+    # path('<str:pk>/', products),
+
+    # path('create/', CreateProduct.as_view()),
+    # path('update/<str:pk>/', UpdateProduct.as_view()),
+    # path('delete/<str:pk>/', DeleteProduct.as_view()),
+
+    path('', include(router.urls)),
+]
+
